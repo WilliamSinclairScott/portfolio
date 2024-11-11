2f23ef04e558d211ad36598f0a2bc44965ab02e8
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-11-11 16:45:59 -0500

    pant pant, refactor, somehow I wasn't using nextjs lol

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,25 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        blink: {
+          '0%, 50%': { opacity: '1' },
+          '51%, 100%': { opacity: '0' },
+        },
+        fadeIn: {
+          from: { opacity: '0', transform: 'translateY(20px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        blink: 'blink 0.7s infinite',
+        fadeIn: 'fadeIn 1s ease-in-out',
+        spin: 'spin 2s linear infinite',
+      },
     },
   },
   plugins: [],
