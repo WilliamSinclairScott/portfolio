7de75072211bbda572b23715053e5d02cfdd7e8b
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-12-24 22:16:56 -0500

    fix login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "./components/LoginForm";
 
 export default function Login() {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen">
       <LoginForm />
     </div>
   );
